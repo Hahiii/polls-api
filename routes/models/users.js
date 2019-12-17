0162e82dd26a5c8c3d130347f042184811fcd955
Nazim Shala <shalanazim@gmail.com>
2019-12-17 14:00:31 +0100

    update users schema with mongoose-validation

--- routes/models/users.js
+++ routes/models/users.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
-
+const uniqueValidator = require('mongoose-unique-validator');
 const usersSchema = new mongoose.Schema({
-    firstname: { type: String },
-    lastname: { type: String },
-    email: { type: String },
-    password: { type: String },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, unique: true, required: true},
+    password: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
     mmodifiedAt: { type: Date, default: Date.now }
 })
-
+usersSchema.plugin(uniqueValidator);
+usersSchema.path('firstname').trim()
+usersSchema.path('lastname').trim()
+usersSchema.path('email').trim()
+usersSchema.path('password').trim()
 module.exports = User = mongoose.model('User', usersSchema);
