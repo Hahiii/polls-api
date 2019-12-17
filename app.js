77037ed0dfe7b491836de0b66ae4413e0d6bed40
Nazim Shala <shalanazim@gmail.com>
2019-12-17 14:52:54 +0100

    set up jsonwebtoken

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const mongoose = require('mongoose');
+const { Secret_KEY } = require('./secret.json');
+var jwt = require('jsonwebtoken');
+var token = jwt.sign({
+    "sub": "login",
+    "admin": true}, Secret_KEY);
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const pollsRouter = require('./routes/API/polls');
+const registrationRouter = require('./routes/API/registration');
+
+const app = express();
+
 mongoose.connect('mongodb://localhost/finalProject', { useNewUrlParser: true });
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var pollsRouter = require('./routes/API/polls');
-var registrationRouter = require('./routes/API/registration');
-
-var app = express();
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
