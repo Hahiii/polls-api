8d6684205e294c2552397c6da585171dfdb487d6
Nazim Shala <shalanazim@gmail.com>
2019-12-18 11:46:41 +0100

    installed cors and changed port

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors')
 const { Secret_KEY } = require('./secret.json');
 var jwt = require('jsonwebtoken');
 var token = jwt.sign({
     "sub": "login",
-    "admin": true}, Secret_KEY);
+    "admin": true
+}, Secret_KEY);
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -18,6 +20,7 @@ const loginRouter = require('./routes/API/login');
 const app = express();
 
 mongoose.connect('mongodb://localhost/finalProject', { useNewUrlParser: true });
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,4 +30,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', pollsRouter, registrationRouter, loginRouter);
+
 module.exports = app;
